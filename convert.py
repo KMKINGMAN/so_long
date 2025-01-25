8f25a9a130f102991567ff9cb2410f82b461c2c5
KMKINGMAN <kingkurkar@gmail.com>
2025-01-25 18:16:15 +0300

    refactor image conversion to use PIL; streamline pixel processing and XPM generation

--- convert.py
+++ convert.py
@@ -1,134 +1,80 @@
-#!/usr/bin/env python3
-
-import png
-import numpy as np
+from PIL import Image
 import sys
 
-np.set_printoptions(threshold=np.inf)
-
-def get_list1():
-	list_keys1 = []
-	for i in range(32, 126):
-		if i != 34:
-			list_keys1.append(f'{chr(i)}')
-	return list_keys1
-
-def get_list2():
-	list_keys2 = []
-	for i in range(32, 126):
-		if i != 34:
-			for j in range(32, 126):
-				if j != 34:
-					list_keys2.append(f'{chr(i)+ chr(j)}')
-	return list_keys2
-
-def get_list3(): #804357 size
-	list_keys3 = []
-	for i in range(32, 126):
-		if i != 34:
-			for j in range(32, 126):
-				if j != 34:
-					for k in range(32, 126):
-						if k != 34:
-							list_keys3.append(f'{chr(i)+ chr(j) + chr(k)}')
-	return list_keys3
-
 def get_colors(pixels):
-	global depth
 	colors_list = []
-	colums = len(pixels)
-	rows = len(pixels[0])
-	for colums in range(colums):
-		i = 0
-		if depth == 4:
-			while i < rows:
-				color = \
-				str(format(pixels[colums][i+3], "02X")) \
-				+ str(format(pixels[colums][i+0], "02X")) \
-				+ str(format(pixels[colums][i+1], "02X")) \
-				+ str(format(pixels[colums][i+2], "02X"))
-				colors_list.append(color)
-				i += depth
-		else:
-			while i < rows:
-				color = \
-				  str(format(pixels[colums][i+0], "02X")) \
-				+ str(format(pixels[colums][i+1], "02X")) \
-				+ str(format(pixels[colums][i+2], "02X"))
-				colors_list.append(color)
-				i += depth
-			
+	height = len(pixels)
+	width = len(pixels[0])  # Remove the division by depth
+	
+	for y in range(height):
+		for x in range(width):
+			pixel = pixels[y][x]  # Access pixel directly
+			if isinstance(pixel, int):  # Handle single channel images
+				color = format(pixel, "02X") * 3
+			else:
+				if len(pixel) == 4:  # RGBA
+					if pixel[3] < 128:  # If mostly transparent
+						colors_list.append('None')
+						continue
+					color = ''.join(format(pixel[j], "02X") for j in range(3))
+				else:  # RGB
+					color = ''.join(format(pixel[j], "02X") for j in range(3))
+			colors_list.append(color)
 	return colors_list
 
-
 def pixels_to_xpm(pixels, filename):
-	final_dict = {}
 	pixels_list = get_colors(pixels)
-	pixels_len = len(set(list(pixels_list)))
-	char_per_pixels = 1
-	colums = len(pixels)
-	rows = round(len(pixels[0]) / depth)
-	if pixels_len <= 93:
-		list_keys = get_list1()
-	elif pixels_len <= 8649:
-		char_per_pixels = 2
-		list_keys = get_list2()
-	else:
-		char_per_pixels = 3
-		list_keys = get_list3()
-	with open(filename, "w") as f:
+	rows = len(pixels)
+	columns = len(pixels[0])
+	
+	# Create a mapping of colors to characters
+	unique_colors = list(set(pixels_list))
+	color_chars = [' ', '.', 'X', 'o', 'O', '+', '@', '#']  # Fixed character set
+	color_map = dict(zip(unique_colors, color_chars[:len(unique_colors)]))
+	
+	# Write XPM file
+	with open(filename, 'w') as f:
 		f.write("/* XPM */\n")
 		f.write("static char *result[] = {\n")
 		f.write("/* columns rows colors chars-per-pixel */\n")
-		nb_keys = 0
-		for pixel in pixels_list:
-			if pixel not in final_dict:
-				final_dict[pixel] = list_keys[nb_keys]
-				nb_keys += 1
-		size_fin_colors = len(final_dict)
-		f.write(f"\"{rows} {colums} {size_fin_colors} {char_per_pixels} \",\n")
-		for col in final_dict:
-			f.write(f"\"{final_dict[col]} c #{col}\",\n")
-		f.write("/* pixels */\n\"")
-		i = 0
-		for color in pixels_list:
-			f.write(str(final_dict[color]))
-			i += 1
-			if i % rows == 0:
-				f.write("\"")
-				if i != colums * rows:
-					f.write(",\n\"")
-				else:
-					f.write("\n};")
-
-depth = 0
-
-def parse_pixels(arg):
-	global depth
-	if len(arg) < 2:
-		print("Usage : 'python3 png_to_xpm.py input_file.png [output_name.xpm]'")
-		return
-	try:
-		with open(arg[1], "rb") as f:
-			reader = png.Reader(f)
-			_, _, pixels, meta = reader.asDirect()
-			if meta['alpha'] == True:
-				depth = 4
+		f.write(f"\"{columns} {rows} {len(unique_colors)} 1 \",\n")
+		
+		# Write color definitions
+		for color, char in color_map.items():
+			if color == 'None':
+				f.write(f"\"{char} c None\",\n")
 			else:
-				depth = 3
-			pixel_array = np.array(list(pixels))
-			if len(arg) == 3:
-				pixels_to_xpm(pixel_array, arg[2])
+				f.write(f"\"{char} c #{color}\",\n")
+		
+		# Write pixel data
+		for y in range(rows):
+			row = ''.join(color_map[pixels_list[y * columns + x]] for x in range(columns))
+			if y == rows - 1:
+				f.write(f"\"{row}\"\n")
 			else:
-				if arg[1][-4:] == "png":
-					pixels_to_xpm(pixel_array, arg[1][:-3] + "xpm")
-				else:
-					pixels_to_xpm(pixel_array, arg[1] + ".xpm")
-	except (IOError, png.FormatError, OSError):
-		print("Input file does not exist or is corrupted png file.")
-
-def main():
-	parse_pixels(sys.argv)
+				f.write(f"\"{row}\",\n")
+		f.write("};\n")
 
 if __name__ == "__main__":
-	main()
+	if len(sys.argv) != 3:
+		print("Usage: python3 convert.py input.png output.xpm")
+		sys.exit(1)
+	
+	input_file = sys.argv[1]
+	output_file = sys.argv[2]
+	
+	# Open and convert the image
+	img = Image.open(input_file)
+	
+	# Convert image to RGBA if it isn't already
+	img = img.convert('RGBA')
+	
+	# Get pixel data as a list
+	pixels = list(img.getdata())
+	
+	# Convert to 2D list with correct dimensions
+	width, height = img.size
+	pixels = [pixels[i:i+width] for i in range(0, len(pixels), width)]
+	
+	# Convert to XPM
+	pixels_to_xpm(pixels, output_file)
